909330ec44bba9bb2491452e92d873c8f9100635
Trung <ductrung4263@gmail.com>
2022-02-25 14:52:43 +0700

    fix url mongoDB

--- services/user.js
+++ services/user.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user');
 
 exports.logIn = async (email) => {
+    console.log("myEmail: ",email);
     const user = await userModel.findOne({email: email},'id email password avatar');
+    console.log('user : ',user);
     return user;
 }
