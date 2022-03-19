9fda24be6d0f687fdb10b304dc14116686845344
Trung <ductrung4263@gmail.com>
2022-03-19 15:37:17 +0700

    update code

--- routes/category.js
+++ routes/category.js
@@ -6,20 +6,21 @@ const { uploadSingleImage } = require('../middlewares/handleImages');
 
 router.get('/', async function (req, res, next) {
     const category = await categoryController.getAll();
-    res.render('category',{category});
+    res.render('category', { category });
+    res.send(category);
 });
 
 router.post('/addCategory', uploadSingleImage.single('thumbnail'), async function (req, res, next) {
-    const category = await categoryController.addCategory({...req.body}, req)
+    const category = await categoryController.addCategory({ ...req.body }, req)
     res.send(category);
 });
 
 
 router.post('/deleteCategory', async (req, res, next) => {
-    const {id} = req.body;
+    const { id } = req.body;
     const data = await categoryController.deleteCategory(id)
-    res.send({data})
-  })
+    res.send({ data })
+})
 
 
 module.exports = router;
