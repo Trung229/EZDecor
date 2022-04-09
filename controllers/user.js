71b79e6191f420e25f89cc204b7d2e8e064d57ff
Trung <ductrung4263@gmail.com>
2022-04-09 13:26:14 +0700

    fix: validate numAuth

--- controllers/user.js
+++ controllers/user.js
@@ -4,63 +4,66 @@ const firebase = require('../db_firebase');
 require('dotenv').config();
 
 
-exports.logIn = async (emailF, passwordF)=>{
+exports.logIn = async(emailF, passwordF) => {
     const user = await userService.logIn(emailF);
     const checkPass = user ? passwordF === user.password : null;
-    if(!user){
+    if (!user) {
         return {
             message: 'user not exist',
             status: false,
         };
-    }else if(!checkPass){
+    } else if (!checkPass) {
         return {
             message: 'Passwords do not match',
             status: false,
         };
     }
-    return {id: user._id, email: user.email, status: true, avatar: user.avatar};
+    return { id: user._id, email: user.email, status: true, avatar: user.avatar };
 }
 
-exports.loginWithThirdParty = async (data)=>{
+exports.loginWithThirdParty = async(data) => {
     const check = await userService.loginWithThirdParty(data);
     return check;
 }
 
-exports.checkEmail =async (email)=>{
+exports.checkEmail = async(email) => {
     const check = await userService.checkEmail(email);
     return check;
 }
 
-exports.register = async (name, email, password, dob, code, phone) =>{
+exports.register = async(name, email, password, dob, code, phone) => {
     const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password,salt);
+    const hash = await bcrypt.hash(password, salt);
     const check = await userService.register(name, email, hash, dob, code, phone);
     return check;
 }
 
-exports.mobileLogin = async (email, password)=>{
+exports.mobileLogin = async(email, password) => {
     const user = await userService.mobileLogIn(email, password);
     const checkPass = await bcrypt.compare(password, user.password);
-    return checkPass;
+    return {
+        checkPass,
+        data: user
+    };
 }
 
-exports.uploadSingleImages = async (req,res)=>{
+exports.uploadSingleImages = async(req, res) => {
     if (!req.file) {
         res.status(400).send("Error: No files found")
     }
     const imageName = req.file;
     const finalName = Date.now() + "." + imageName.originalname.split(".").pop();
     const blob = firebase.bucket.file(finalName);
-    return new Promise((resolve, reject) =>{
+    return new Promise((resolve, reject) => {
         const blobWriter = blob.createWriteStream({
             metadata: {
                 contentType: imageName.mimetype
             }
         });
-         blobWriter.on('error', (err) => {
+        blobWriter.on('error', (err) => {
             reject(err);
         });
-         blobWriter.on('finish', async () => {
+        blobWriter.on('finish', async() => {
             await blob.makePublic()
             req.file.firebaseUr = `https://storage.googleapis.com/${process.env.STORAGE_BUCKET}/${finalName}`
             resolve(req.file.firebaseUr)
@@ -72,6 +75,6 @@ exports.uploadSingleImages = async (req,res)=>{
 
 }
 
-exports.getAllUsers = async () =>{
+exports.getAllUsers = async() => {
     return await userService.getAllUsers();
 }
